e40b36a21da3308cf60fef1036b3ebc23053e198
Filip Chovanec <filip@chovanec.com>
2023-12-24 23:40:26 +0100

    Test model.rs

--- app/model.rs
+++ app/model.rs
@@ -4,19 +4,38 @@ pub struct Model {
     pub score: RwLock<usize>,
 }
 
+const START_SCORE: usize = 0;
+
 impl Model {
     pub fn new() -> Self {
         Self {
-            score: RwLock::new(0),
+            score: RwLock::new(START_SCORE),
         }
     }
 
     pub fn increment_score(&self) {
         let mut x = self.score.write().unwrap();
         *x += 1;
+        // Prevent integer overflow
+        if *x == std::usize::MAX {
+            *x = 0;
+        }
     }
 
     pub fn get_score(&self) -> usize {
         *self.score.read().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn scoring() {
+        let model = Model::new();
+        assert_eq!(model.get_score(), START_SCORE);
+        model.increment_score();
+        assert_eq!(model.get_score(), START_SCORE + 1);
     }
 }
