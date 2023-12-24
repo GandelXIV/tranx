2b627dd7fea1f1fa847aea6d41eafb944aa80c45
Filip Chovanec <filip@chovanec.com>
2023-12-25 00:36:14 +0100

    Add example comments functionality

--- app/model.rs
+++ app/model.rs
@@ -2,6 +2,7 @@ use std::sync::RwLock;
 
 pub struct Model {
     pub score: RwLock<usize>,
+    comments: RwLock<Vec<String>>,
 }
 
 const START_SCORE: usize = 0;
@@ -10,6 +11,7 @@ impl Model {
     pub fn new() -> Self {
         Self {
             score: RwLock::new(START_SCORE),
+            comments: RwLock::new(vec!["Amazing website!".to_string()]),
         }
     }
 
@@ -24,6 +26,14 @@ impl Model {
 
     pub fn get_score(&self) -> usize {
         *self.score.read().unwrap()
+    }
+
+    pub fn get_comments(&self) -> std::sync::RwLockReadGuard<'_, Vec<String>> {
+        self.comments.read().unwrap()
+    }
+
+    pub fn new_comment(&self, text: String) {
+        self.comments.write().unwrap().push(text);
     }
 }
 
--- app/routes.rs
+++ app/routes.rs
@@ -7,8 +7,9 @@ use axum::{
     extract::State,
     response::Html,
     routing::{get, post},
-    Router,
+    Form, Router,
 };
+use serde::Deserialize;
 use std::sync::Arc;
 use tera::Context;
 
@@ -16,6 +17,7 @@ pub fn build_routes() -> Router {
     Router::new()
         .route("/", get(hello))
         .route("/increment", post(increment))
+        .route("/comments", post(comments))
         .with_state(Arc::new(model::Model::new()))
 }
 
@@ -24,7 +26,22 @@ pub fn build_routes() -> Router {
 async fn hello(State(model): State<Arc<Model>>) -> Html<String> {
     views::render_template(
         "pages/greet.html",
-        &context! { name => "World", score => &model.get_score()},
+        &context! { name => "World", score => &model.get_score(), comments => &*model.get_comments()},
+    )
+}
+
+#[derive(Deserialize)]
+struct CommentData {
+    newcomment: Option<String>,
+}
+
+async fn comments(State(model): State<Arc<Model>>, Form(data): Form<CommentData>) -> Html<String> {
+    if let Some(newcomment) = data.newcomment {
+        model.new_comment(newcomment.clone());
+    }
+    views::render_template(
+        "components/comments.html",
+        &context! { comments => &*model.get_comments() },
     )
 }
 
